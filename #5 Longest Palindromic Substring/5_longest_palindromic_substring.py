637855d0b1ac48f7ee28231cb1bef1f1b3908c4e
Loges <logiflash99037@gmail.com>
2022-12-10 10:49:48 +0530

    Update 5_longest_palindromic_substring.py

--- #5 Longest Palindromic Substring/5_longest_palindromic_substring.py
+++ #5 Longest Palindromic Substring/5_longest_palindromic_substring.py
@@ -30,3 +30,18 @@ s consist of only digits and English letters.
 """
 
 ## Solution 1
+class Solution:
+    def palindrome(self, s: str, left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left+1:right]
+
+    def longestPalindrome(self, s: str) -> str:
+        longest_sub_s = ''
+        for i in range(len(s)):
+            s1 = self.palindrome(s, i, i) # For odd len substring
+            s2 = self.palindrome(s, i, i+1) # For even len substring
+            print(s1, s2)
+            longest_sub_s = max([longest_sub_s, s1, s2], key=len)
+        return longest_sub_s
