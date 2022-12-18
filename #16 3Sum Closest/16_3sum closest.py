d69aa2ae77e6f2e80f5186824feed6ab29629ac3
Loges <logiflash99037@gmail.com>
2022-12-18 09:37:33 +0530

    Create 16_3sum closest.py

---
+++ #16 3Sum Closest/16_3sum closest.py
@@ -0,0 +1,59 @@
+"""
+
+---
+MEDUM
+14. 3Sum Closest
+---
+
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+Return the sum of the three integers.
+
+You may assume that each input would have exactly one solution.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ 
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+
+"""
+
+## Solution 1
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        res = nums[0] + nums[1] + nums[2]
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+            num = nums[i]
+            while left < right:
+                curr_sum = nums[left] + nums[right] + num
+                if curr_sum == target:
+                    return curr_sum
+                if curr_sum < target:
+                    left += 1
+                else: # summed > target
+                    right -= 1
+                if abs(curr_sum - target) < abs(res - target):
+                    res = curr_sum
+        return res
