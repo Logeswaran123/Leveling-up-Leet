3877a0984f07946815f8f94b30300bf41bda76a0
Loges <logiflash99037@gmail.com>
2022-12-18 09:39:58 +0530

    Update 16_3sum closest.py

--- #16 3Sum Closest/16_3sum closest.py
+++ #16 3Sum Closest/16_3sum closest.py
@@ -2,7 +2,7 @@
 
 ---
 MEDUM
-14. 3Sum Closest
+16. 3Sum Closest
 ---
 
 Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
