ab7cde10faa3f2b30456d19edd0dc9aa063cf786
Loges <logiflash99037@gmail.com>
2024-09-05 09:14:05 -0700

    Add 271

---
+++ #271 Encode and Decode Strings/271_encode_and_decode_strings.py
@@ -0,0 +1,49 @@
+"""
+
+---
+MEDIUM
+271. Encode and Decode Strings
+---
+
+Design an algorithm to encode a list of strings to a single string. The encoded string is then decoded back to the original list of strings.
+
+Please implement encode and decode
+
+Example 1:
+
+Input: ["neet","code","love","you"]
+
+Output:["neet","code","love","you"]
+Example 2:
+
+Input: ["we","say",":","yes"]
+
+Output: ["we","say",":","yes"]
+Constraints:
+
+0 <= strs.length < 100
+0 <= strs[i].length < 200
+strs[i] contains only UTF-8 characters.
+
+
+"""
+
+## Solution 1
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for s in strs:
+            encoded += str(len(s)) + "#" + s
+        return encoded
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#" and s[j].isdigit():
+                j+=1
+            length = int(s[i:j])
+            decoded.append(s[j+1:j+1+length])
+            i = j+1+length
+        return decoded
