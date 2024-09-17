18fdb0a41af53e359c69af19fa2425b9333f8bd8
Logeswaran Sivakumar <loges.siva14@gmail.com>
2024-09-16 17:49:52 -0700

    Update 200_number_of_islands.py

--- #200 Number of Islands/200_number_of_islands.py
+++ #200 Number of Islands/200_number_of_islands.py
@@ -41,7 +41,7 @@ grid[i][j] is '0' or '1'.
 """
 
 ## Solution 1
-# Solution with lambda function and Recursive DFS. Good to look at.
+# Solution with Recursive DFS. Good to look at.
 #
 # Logic:
 # Input grid:
