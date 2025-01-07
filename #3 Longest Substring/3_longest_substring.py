0178c2d475cbb5cfe11bedc33965699d5e047f6c
Loges <logiflash99037@gmail.com>
2025-01-06 23:57:18 -0800

    Update 3_longest_substring.py

--- #3 Longest Substring/3_longest_substring.py
+++ #3 Longest Substring/3_longest_substring.py
@@ -56,12 +56,43 @@ class Solution:
 ### Sliding Window method
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        charset = set()
-        window_left, longest_length = 0, 0
-        for window_right in range(len(s)):
-            while s[window_right] in charset:
-                charset.remove(s[window_left])
-                window_left += 1  
-            charset.add(s[window_right])
-            longest_length = max(longest_length, window_right - window_left + 1)
-        return longest_length 
+        char_set = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in char_set:
+                char_set.remove(s[l])
+                l += 1
+            char_set.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+
+## Solution 3
+### Sliding Window Optimized
+# This function finds the length of the longest substring in a given string s 
+# that contains only unique characters. 
+# It uses a sliding window approach with two pointers (l and r) and a dictionary (last_seen_idx) 
+# to keep track of the last seen indices of characters.
+# The steps are:
+# 1. Expand the right pointer (r) to iterate through the string.
+# 2. If a duplicate character is found, move the left pointer (l) to exclude the previous occurrence.
+#       Specifically, set l to the maximum of:
+#           The next index after the last occurrence of s[r] (last_seen_idx[s[r]] + 1).
+#           The current l, to ensure it doesn't move backward.
+# 3. Update the dictionary with the current character's index.
+# 4. Calculate the current substring length and update the result (res) if it's larger.
+# 5. Return the maximum length of a substring with unique characters.
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        last_seen_idx = {}
+        l = 0
+        res = 0
+        
+        for r in range(len(s)):
+            if s[r] in last_seen_idx:
+                l = max(last_seen_idx[s[r]] + 1, l)
+            last_seen_idx[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
